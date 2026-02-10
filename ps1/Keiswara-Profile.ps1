0b3a1e3a63ba9f77a07c88f3edfcdd525fbf8e2b
keiswara61 <keiswara61@gmail.com>
2026-02-10 11:36:51 +0700

    Update Keiswara-Profile.ps1

--- ps1/Keiswara-Profile.ps1
+++ ps1/Keiswara-Profile.ps1
@@ -1,2 +1,3 @@
-param ($Scale, $Name)
+param ([string]$Scale, [string]$Name)
+
 magick -size 7x7 xc:snow -fill SkyBlue4 -draw 'point 2,2 point 4,2 point 2,3 point 3,3 point 2,4 point 4,4' -scale $Scale $Name
