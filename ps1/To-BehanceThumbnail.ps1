13b3c96db3faf8f5478d47d9d643c81d75d3c970
keiswara61 <keiswara61@gmail.com>
2026-02-13 16:52:12 +0700

    Update To-BehanceThumbnail.ps1

--- ps1/To-BehanceThumbnail.ps1
+++ ps1/To-BehanceThumbnail.ps1
@@ -1,14 +1,9 @@
-param ([string]$InputName, [string]$RGBPalletes)
+param ([string]$InputName)
 
 [string]$LeafBase = $InputName.Split("\.")[0]
 [string]$Extension = $InputName.Split("\.")[1]
 [int]$Width = 808
 [int]$Height = 632
-[int]$PalletesCount = $RGBPalletes.Split(",").Count
-[int]$DrawWidth = $Width - $Height
-
-$RGBPalletes.Split(",").Trim() | ForEach-Object -Begin {[string]$Draw = $Null; [int]$Index = 0} -Process {$Draw += "fill rgb($($_.Replace(" ", ", "))) point 0,$($Index) "; $Index++}
 
 magick $InputName -resize "x$($Height)" "$($LeafBase)-Resize.$($Extension)"
-magick -size "1x$($PalletesCount)" xc:snow -draw $Draw -scale "$($DrawWidth)x$($Height)!" "$($LeafBase)-Draw.$($Extension)"
-magick "$($LeafBase)-Resize.$($Extension)" "$($LeafBase)-Draw.$($Extension)" +append "$($LeafBase)-Thumbnail.$($Extension)"
+magick "$($LeafBase)-Resize.$($Extension)" -background snow -gravity center -extent "$($Width)x" "$($LeafBase)-Thumbnail.$($Extension)"
