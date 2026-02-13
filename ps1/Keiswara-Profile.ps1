bc9f083fc20d540738802cf8fca70670e27328a0
keiswara61 <keiswara61@gmail.com>
2026-02-13 10:21:14 +0700

    Update Keiswara-Profile.ps1

--- ps1/Keiswara-Profile.ps1
+++ ps1/Keiswara-Profile.ps1
@@ -1,10 +1,10 @@
 param ([string]$Scale, [int]$HeightZoomPercentage, [string]$OutputName)
 
-[int]$ScaleWidth = ($Scale -split "x")[0]
-[int]$ScaleHeight = ($Scale -split "x")[1]
+[int]$ScaleWidth = $Scale.Split("x")[0]
+[int]$ScaleHeight = $Scale.Split("x")[1]
 [int]$HeightZoom = $ScaleHeight * $HeightZoomPercentage / 100
-[string]$LeafBase = ($OutputName -split "\.")[0]
-[string]$Extension = ($OutputName -split "\.")[1]
+[string]$LeafBase = $OutputName.Split("\.")[0]
+[string]$Extension = $OutputName.Split("\.")[1]
 
 magick -size 5x5 xc:snow -fill skyblue4 -draw "point 1,1 point 3,1 point 1,2 point 2,2 point 1,3 point 3,3" -scale "x$($HeightZoom)" "$($LeafBase)-Draw.$($Extension)"
 magick "$($LeafBase)-Draw.$($Extension)" -background snow -gravity center -extent $Scale $OutputName
