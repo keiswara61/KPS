0b197ddacdd7b0b214cf3efe745bc35878589061
keiswara61 <keiswara61@gmail.com>
2026-02-13 17:04:42 +0700

    Delete ps1/To-BehanceThumbnail.ps1

--- ps1/To-BehanceThumbnail.ps1
+++
@@ -1,9 +0,0 @@
-param ([string]$InputName)
-
-[string]$LeafBase = $InputName.Split("\.")[0]
-[string]$Extension = $InputName.Split("\.")[1]
-[int]$Width = 808
-[int]$Height = 632
-
-magick $InputName -resize "x$($Height)" "$($LeafBase)-Resize.$($Extension)"
-magick "$($LeafBase)-Resize.$($Extension)" -background snow -gravity center -extent "$($Width)x" "$($LeafBase)-BehanceThumbnail.$($Extension)"
